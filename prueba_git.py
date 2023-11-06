81edbe0f90e2662b7ed02cff41bbeb5eab74660f
Marcelo Luis Vanni <mlvcor32@gmail.com>
2023-11-06 00:25:03 -0300

    me la mande pero reinicie y agregué otra función

--- prueba_git.py
+++ prueba_git.py
@@ -7,3 +7,9 @@ print("afregando para ver")
 
 def sumar():
     ...
+def multiplo(a, b):
+    if a % b == 0:
+        mensaje = f"es multiplo"
+        return mensaje
+    else:
+        return "no es multiplo"
